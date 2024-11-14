6d52ddb7d8b99cbf0eccf2c2c60d5106999a440b
Malcolm Lett <malcolm.lett@gmail.com>
2024-11-14 09:00:14 +0530

    First cut of predict_at_location().
    Not tested.

--- slam_motion.py
+++ slam_motion.py
@@ -1,9 +1,88 @@
 # Animates motion of the SLAM agent.
 
+# TODO rename to "slam_operations" so I can use it for static application of the slam model etc.
+
+import lds
+import slam_data
+
 import numpy as np
 import cv2
 import math
-import lds
+import tensorflow as tf
+
+
+def predict_at_location(full_map, known_map, known_map_start, model, location, orientation, **kwargs):
+    """
+
+    Args:
+        full_map: semantic map of entire floorplan
+            Used for LDS map generation.
+        known_map: known semantic map
+            Used for input map generation.
+        known_map_start: (x,y), units: physical
+            Coordinate of top-left corner of known_map
+        model: model for semantic map prediction.
+            Or omit to exclude semantic_maps from the output.
+        location: (x,y) float, units: physical
+        orientation: float, unit: radians
+
+    Keyword args:
+        pixel_size: the usual
+        max_distance: the usual
+
+    Return:
+        (map_prediction, accept, delta (x,y), delta orientation)
+        - map_prediction: tensor (H,W,3)
+        - accept: bool
+        - delta (x,y): (2,) float numpy array, units: physical
+        - delta orientation: float, unit: radians
+    """
+    # Config
+    pixel_size = kwargs.get('pixel_size', lds.__PIXEL_SIZE__)
+    max_distance = kwargs.get('max_distance', lds.__MAX_DISTANCE__)
+
+    # Coords
+    # - just like in generate_training_data_sample(), we align to map pixels
+    #   when rendering
+    window_size_px = np.ceil(max_distance / pixel_size).astype(int) * 2 + 1
+    window_radius_px = (window_size_px - 1) // 2
+    window_size_px = np.array([window_size_px, window_size_px])
+
+    # Extract input map from known map
+    # - align to known_map pixels, and fill the rest with 'unknown'
+    unknown_value = np.zeros(slam_data.__CLASSES__, dtype=np.float32)
+    unknown_value[slam_data.__UNKNOWN_IDX__] = 1
+    map_window = tf.tile(unknown_value, multiples=(window_size_px, window_size_px, 1))
+
+    centre_fpx = (location - known_map_start) / pixel_size
+    start_fpx = centre_fpx - window_radius_px
+    start_px = np.round(start_fpx).astype(int)
+    loc_alignment_offset_fpx = start_fpx - start_px
+    known_map_indices, map_window_indices = slam_data.get_intersect_ranges_tf(
+        tf.shape(known_map), tf.shape(map_window), start_px)
+    tf.tensor_scatter_nd_update(map_window, map_window_indices, tf.gather_nd(known_map, known_map_indices))
+
+    # Generate LDS input from full map
+    # - re-use input alignment
+    occupancy_map = full_map[..., slam_data.__OBSTRUCTION_IDX__]
+    ranges = lds.lds_sample(occupancy_map, location, orientation, **kwargs)
+    lds_map = lds.lds_to_occupancy_map(
+        ranges, angle=orientation, size_px=window_size_px, centre_px=loc_alignment_offset_fpx, pixel_size=pixel_size)
+
+    # Do prediction
+    # - turn into TF batch
+    map_input = tf.expand_dims(map_window, axis=0)
+    lds_input = tf.expand_dims(lds_map, axis=0)
+    (map_output, adlo_output) = model.predict((map_input, lds_input))
+
+    # Convert to result
+    # TODO cast to float32
+    map_pred = map_output[0]
+    accept = adlo_output[0][0] >= 0.5
+    delta_location = adlo_output[0][1:3] * window_size_px * pixel_size
+    delta_orientation = adlo_output[0][3] * np.pi
+
+    return map_pred, accept, delta_location, delta_orientation
 
 
 def get_contour_pxcoords(file, **kwargs):
